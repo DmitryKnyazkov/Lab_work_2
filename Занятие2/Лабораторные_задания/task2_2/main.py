b9aa5f9088496d2aa2944850d84dc54f6e02d014
DmitryKnyazkov <kde.2015@yandex.ru>
2022-11-30 20:47:06 +0300

    LR2_task2_2 Done

--- Занятие2/Лабораторные_задания/task2_2/main.py
+++ Занятие2/Лабораторные_задания/task2_2/main.py
@@ -1,6 +1,8 @@
 def count(start_number: float = 1, step: float = 1):
-    ...  # TODO написать функцию-генератор возвращающую целые числа
+    while start_number:
 
+        yield  start_number   # TODO написать функцию-генератор возвращающую целые числа
+        start_number += step
 
 if __name__ == "__main__":
     my_count = count(10, 0.5)
