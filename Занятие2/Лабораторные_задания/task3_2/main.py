36f07434e44e88fd916ae54f244fb0634f5dcb72
DmitryKnyazkov <kde.2015@yandex.ru>
2022-12-01 15:34:45 +0300

    LR2_task3_2 Done

--- Занятие2/Лабораторные_задания/task3_2/main.py
+++ Занятие2/Лабораторные_задания/task3_2/main.py
@@ -1,10 +1,16 @@
 def min_len_check(fn):
+    def wrapper(arg):
+        if len(arg) < 10:
+            raise ValueError ("Строка слишком короткая")
+        result = fn(arg)
+        return result
     # TODO записать обертку для исходной функции
 
     return wrapper
 
 
 # TODO задекорировать функцию
+@min_len_check
 def some_func(str_arg: str):
     ...
 
